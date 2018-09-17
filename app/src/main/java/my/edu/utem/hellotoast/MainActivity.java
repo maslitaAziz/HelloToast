982f4709aa00a92c070f7c0bdc48a22ee367edc7
PSTP-MacbookAir3 <pstp-macbookair3@PSTP-MacbookAir3s-MacBook-Air.local>
2018-09-17 14:06:59 +0800

    Exercise Hello Toast

---
+++ app/src/main/java/my/edu/utem/hellotoast/MainActivity.java
@@ -0,0 +1,35 @@
+package my.edu.utem.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+
+        // What happens when user clicks on the button_count Button goes here.
+        mCount++;
+        if(mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void showToast(View view) {
+
+        // What happens when user clicks on the button_toast Button goes here
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
